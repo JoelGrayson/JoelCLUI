cbb473e41605923c20f1d1a9cae17b8a4874f5b8
JoelGrayson <joel@joelgrayson.com>
2022-04-11 20:41:12 -0400

    Setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='jclui',  
+    version='1.0.5',
+    author="Joel Grayson",
+    author_email="joel@joelgrayson.com",
+    description="A command line user interface tool",
+    long_description='A command line user interface tool with arrow navigation, color management, bold, underlining, etc. with the usage of [bracket tags][/]',
+    url="https://github.com/JoelGrayson/jclui",
+    packages=None,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
