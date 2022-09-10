2ec0c62c1169cda36c61df6eb0a581e72c44504f
Joel Grayson <joel@joelgrayson.com>
2022-09-10 10:14:22 -0400

    Refactoring

---
+++ src/joelclui/constants.py
@@ -0,0 +1,39 @@
+style={
+    'reset': '\033[00m',
+    'clear': '\033[00m',
+    '/': '\033[00m',
+    'default': '\033[00m',
+    'bold': '\033[01m',
+    'underline': '\033[04m'
+}
+txt={ #colors
+    'green': '\033[92m',
+    'success': '\033[92m',
+    'red': '\033[91m',
+    'danger': '\033[91m',
+    'yellow': '\033[93m',
+    'warn': '\033[93m',
+    'blue': '\033[94m',
+    'black': '\033[30m',
+    'orange': '\033[33m',
+    'purple': '\033[35m',
+    'cyan': '\033[36m',
+    'gray': '\033[37m',
+    'lightgray': '\033[37m',
+    'darkgray': '\033[90m',
+    'lightred': '\033[91m',
+    'lightgreen': '\033[92m',
+    'lightblue': '\033[94m',
+    'pink': '\033[95m',
+    'lightcyan': '\033[96m'
+}
+bg={ #colors
+    'bg black': '\033[40m',
+    'bg red': '\033[41m',
+    'bg green': '\033[42m',
+    'bg orange': '\033[43m',
+    'bg blue': '\033[44m',
+    'bg purple': '\033[45m',
+    'bg cyan': '\033[46m',
+    'bg lightgray': '\033[47m',
+}
--- src/joelclui/joelclui.py
+++ src/joelclui/joelclui.py
@@ -1,44 +1,6 @@
 import re
 import sys
-
-style={
-    'reset': '\033[00m',
-    'clear': '\033[00m',
-    '/': '\033[00m',
-    'default': '\033[00m',
-    'bold': '\033[01m',
-    'underline': '\033[04m'
-}
-txt={ #colors
-    'green': '\033[92m',
-    'success': '\033[92m',
-    'red': '\033[91m',
-    'danger': '\033[91m',
-    'yellow': '\033[93m',
-    'warn': '\033[93m',
-    'blue': '\033[94m',
-    'black': '\033[30m',
-    'orange': '\033[33m',
-    'purple': '\033[35m',
-    'cyan': '\033[36m',
-    'lightgray': '\033[37m',
-    'darkgray': '\033[90m',
-    'lightred': '\033[91m',
-    'lightgreen': '\033[92m',
-    'lightblue': '\033[94m',
-    'pink': '\033[95m',
-    'lightcyan': '\033[96m'
-}
-bg={ #colors
-    'bg black': '\033[40m',
-    'bg red': '\033[41m',
-    'bg green': '\033[42m',
-    'bg orange': '\033[43m',
-    'bg blue': '\033[44m',
-    'bg purple': '\033[45m',
-    'bg cyan': '\033[46m',
-    'bg lightgray': '\033[47m',
-}
+from constants import style, txt, bg
 
 system_print=print #store print() before redefining
 
@@ -54,28 +16,19 @@ def print(u_str):
         u_char=matched.groups()[0] #.group() is just '['+color+']'
         start_i=matched.start()
         end_i=matched.end()
-        if 'bg ' in u_char:
+        if 'bg ' in u_char: #background value
             u_str=u_str[0:start_i]+bg[u_char]+u_str[end_i:]
-        elif u_char in list(txt):
+        elif u_char in list(txt): #text value
             u_str=u_str[0:start_i]+txt[u_char]+u_str[end_i:]
-        elif u_char in list(style):
+        elif u_char in list(style): #style value
             u_str=u_str[0:start_i]+style[u_char]+u_str[end_i:]
-        else:
+        else: #unknown value
             raise Exception('Unknown [symbol] matching: '+u_char)
         
-        matchedVal=matched.group()
         matched.start()
     
     system_print(u_str)
 
-def test():
-    print('2+2=[green]4[/] and 3+3=[bg red]6[/]')
-    print('[yellow]Warning. [red][underline]Error[/] [green]Success[/]')
-    print('[bg blue][yellow]Colorful[/] Back to normal')
-
-    print('Status: TBD')
-    move_up()
-    print('[bold]Status: [green]Complete[/]')
 
 def move_up(times=1):
     sys.stdout.write(f'\x1b[{times}A')
@@ -93,5 +46,12 @@ def move_right(times=1):
     sys.stdout.write(f'\x1b[{times}C')
     sys.stdout.flush()
 
-def read_line():
-    return sys.stdout.readline(0)
+
+def test():
+    print('2+2=[green]4[/] and 3+3=[bg red]6[/]')
+    print('[yellow]Warning. [red][underline]Error[/] [green]Success[/]')
+    print('[bg blue][yellow]Colorful[/] Back to normal')
+
+    print('Status: TBD')
+    move_up()
+    print('[bold]Status: [green]Complete[/]')
