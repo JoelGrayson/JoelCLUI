8a6b5ae9eed72a0b738f8a98f318303c8361a998
Joel Grayson <joel@joelgrayson.com>
2022-10-09 20:32:44 -0400

    Build script asks for new version

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Note: Update pyproject.toml's version before running build
+echo "-----Update pyproject.toml's version-----"
+echo -n "Old version: "
+grep version < ./pyproject.toml | sed -E 's/^version ?= ?\"(.*)\"$/\1/'
+echo -n "New version: "
+read -r new_version
+sed "s/^version = .*$/version = \"$new_version\"/" pyproject.toml > temp
+cat temp > pyproject.toml
+rm temp
 
+echo "-----Build-----"
 rm -rf dist
 rm -rf src/joelclui.egg-info
 python3 -m build
